f185475d87769ad8145830a395fa339d9e95be25
Felipe Martins <automacao1@reluzcontabil.com.br>
2026-03-18 09:37:02 -0300

    quinto commit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
+  reactStrictMode: true,
   output: 'export',
+  assetPrefix: './',
+  basePath: '',
 };
 
 export default nextConfig;
-
-const nextConfig = {
-  output: 'export',
-  basePath: '',
-  assetPrefix: './',
-};
